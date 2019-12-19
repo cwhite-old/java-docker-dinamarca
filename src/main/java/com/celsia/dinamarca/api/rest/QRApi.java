b6dcc0a9f3b175f8bb59b4c4a31af6b8049360de
Azurebird <white.camilo@gmail.com>
2019-12-19 12:09:23 -0500

    Conflicts fixed

--- src/main/java/com/celsia/dinamarca/api/rest/QRApi.java
+++ src/main/java/com/celsia/dinamarca/api/rest/QRApi.java
@@ -1,13 +1,11 @@
 package com.celsia.dinamarca.api.rest;
 
-<<<<<<< HEAD
 import com.celsia.dinamarca.config.Documented;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-=======
 import com.celsia.dinamarca.api.rest.entity.QRContent;
 import com.celsia.dinamarca.repository.model.QRModel;
 import com.celsia.dinamarca.service.QRService;
@@ -15,18 +13,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
->>>>>>> origin/dev
 
 import java.util.Collection;
 import java.util.Collections;
 
-<<<<<<< HEAD
 @Documented
-@RestController("/qr-api/v1/codes/{device}")
-=======
 @RestController
 @RequestMapping("/qr-api/v1/codes/{device}")
->>>>>>> origin/dev
 public final class QRApi {
 
     private QRService qrService;
