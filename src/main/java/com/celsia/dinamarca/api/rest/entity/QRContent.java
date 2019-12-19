c6689ce49b480b730a8099ff0d6905876817e414
Azurebird <white.camilo@gmail.com>
2019-12-19 11:35:00 -0500

    Mapping now working

--- src/main/java/com/celsia/dinamarca/api/rest/entity/QRContent.java
+++ src/main/java/com/celsia/dinamarca/api/rest/entity/QRContent.java
@@ -1,10 +1,31 @@
 package com.celsia.dinamarca.api.rest.entity;
 
-import lombok.Data;
-
-@Data
 public class QRContent {
     private String imei;
     private String location;
     private String qr;
+
+    public String getImei() {
+        return imei;
+    }
+
+    public void setImei(String imei) {
+        this.imei = imei;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getQr() {
+        return qr;
+    }
+
+    public void setQr(String qr) {
+        this.qr = qr;
+    }
 }
