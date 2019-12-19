425f7461ef631d9286e73e5b7e8fc6aa5e43bcce
Jair Tamayo <jair.tamayo@gmail.com>
2019-12-19 08:20:41 -0600

    initialized spring boot application

---
+++ src/test/java/com/celsia/dinamarca/DinamarcaServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.celsia.dinamarca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinamarcaServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
